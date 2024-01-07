1c3a1350718a8cb48125e6fb3fb0e7a4dea65d64
chobolevel <rodaka123@naver.com>
2024-01-07 13:05:33 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("render");
+  useEffect(() => console.log("i run only once."), []);
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
