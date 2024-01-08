e8fa65e5142abd281ce02abfc240c4f03e4d6b70
chobolevel <rodaka123@naver.com>
2024-01-08 15:13:30 +0900

    #7.1, 7.2 To Do List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  function byeFn() {
-    console.log("destroyed");
-  }
-  function hiFn() {
-    console.log("created");
-    return byeFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!toDo) {
+      return;
+    }
+    setToDos((cur) => [...cur, toDo]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Writer your to do..."
+          value={toDo}
+          onChange={onChange}
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, idx) => (
+          <li key={idx}>{toDo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
