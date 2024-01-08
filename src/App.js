f2d2b64e7754d1f31e051bca64b643855ad49be3
chobolevel <rodaka123@naver.com>
2024-01-08 16:02:17 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (!toDo) {
-      return;
-    }
-    setToDos((cur) => [...cur, toDo]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(1);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins([...json]);
+        setLoading(false);
+      });
+  }, []);
+  const onChange = (e) => setAmount(e.target.value);
+  const getShare = (price) => {
+    return amount / Math.round(price);
   };
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Writer your to do..."
-          value={toDo}
-          onChange={onChange}
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((toDo, idx) => (
-          <li key={idx}>{toDo}</li>
-        ))}
-      </ul>
+      <h1>The Coins!</h1>
+      {loading ? (
+        <b>Loading ...</b>
+      ) : (
+        <>
+          <label htmlFor="amount">How many do you have in USD</label>
+          <br />
+          <input type="number" id="amount" value={amount} onChange={onChange} />
+          <hr />
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): $
+                {Math.round(coin.quotes.USD.price)} USD (can buy about{" "}
+                {getShare(coin.quotes.USD.price)} {coin.symbol})
+              </option>
+            ))}
+          </select>
+        </>
+      )}
     </div>
   );
 }
