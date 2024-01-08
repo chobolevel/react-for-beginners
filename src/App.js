db5b206dc4f5c736c3120fc3d5ba26beebfc0b1b
chobolevel <rodaka123@naver.com>
2024-01-08 16:48:39 +0900

    #7.3 Movie App Part One

--- src/App.js
+++ src/App.js
@@ -2,41 +2,37 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(1);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((res) => res.json())
-      .then((json) => {
-        setCoins([...json]);
-        setLoading(false);
-      });
-  }, []);
-  const onChange = (e) => setAmount(e.target.value);
-  const getShare = (price) => {
-    return amount / Math.round(price);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const res = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    );
+    const json = await res.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      <h1>The Coins!</h1>
       {loading ? (
-        <b>Loading ...</b>
+        <h1>Loading ...</h1>
       ) : (
-        <>
-          <label htmlFor="amount">How many do you have in USD</label>
-          <br />
-          <input type="number" id="amount" value={amount} onChange={onChange} />
-          <hr />
-          <select>
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}): $
-                {Math.round(coin.quotes.USD.price)} USD (can buy about{" "}
-                {getShare(coin.quotes.USD.price)} {coin.symbol})
-              </option>
-            ))}
-          </select>
-        </>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
