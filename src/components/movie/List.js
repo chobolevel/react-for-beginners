60adfdfeedb488de76de8949f07a47c5cb3bccc4
chobolevel <rodaka123@naver.com>
2024-01-15 15:33:00 +0900

    Feat: add movie component

--- src/components/movie/List.js
+++ src/components/movie/List.js
@@ -1,3 +1,19 @@
-function List() {
-  return <div></div>;
+import ListItem from "./ListItem";
+import Movie from "../../classes/Movie";
+import PropTypes from "prop-types";
+
+List.propTypes = {
+  movieList: PropTypes.arrayOf(Movie).isRequired,
+};
+
+function List({ movieList }) {
+  return (
+    <div>
+      {movieList.map((movie) => (
+        <ListItem key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 }
+
+export default List;
--- src/components/movie/ListItem.js
+++ src/components/movie/ListItem.js
@@ -1,3 +1,12 @@
-function ListItem() {
-  return <div></div>;
+function ListItem({ movie }) {
+  return (
+    <div>
+      <img
+        src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.posterPath}`}
+        alt="포스터"
+      />
+    </div>
+  );
 }
+
+export default ListItem;
