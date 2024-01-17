a0d0db32272375c75d1bc83e160d5e72c241ad29
chobolevel <rodaka123@naver.com>
2024-01-18 01:15:52 +0900

    Refactor: add style

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import Header from "./components/partials/Header";
 import Footer from "./components/partials/Footer";
+import "./styles/init.css";
+import "./styles/styles.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isTop, setIsTop] = useState(true);
+  const handleScroll = (e) => {
+    if (window.scrollY > 0) {
+      setIsTop(false);
+    } else {
+      setIsTop(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route path="/movie/:id" element={<Detail />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <Header isTop={isTop} />
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </>
   );
