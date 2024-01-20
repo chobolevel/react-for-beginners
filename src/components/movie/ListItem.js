b879b4597294211103ad0857f202ae238f83ede6
chobolevel <rodaka123@naver.com>
2024-01-20 17:47:35 +0900

    Refactor: add mouse func

--- src/components/movie/ListItem.js
+++ src/components/movie/ListItem.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import ModalPortal from "../../Portal";
+
 function ListItem({ movie }) {
+  const [timeoutVar, setTimeoutVar] = useState(null);
+  const [isActive, setIsActive] = useState(false);
+  const handleMouseEnter = () => {
+    const timeout = setTimeout(() => {
+      setIsActive(true);
+    }, 1000);
+    setTimeoutVar(timeout);
+  };
+  const handleMouseLeave = () => {
+    clearTimeout(timeoutVar);
+    setIsActive(false);
+  };
   return (
     <div className="movie-list-item">
       <img
         className="movie-poster"
         src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.posterPath}`}
         alt="포스터"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       />
+      {isActive ? (
+        <ModalPortal>
+          <h1>hi</h1>
+        </ModalPortal>
+      ) : null}
     </div>
   );
 }
