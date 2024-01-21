535d884ef883d1d1854bee7b786c5891c5076f92
chobolevel <rodaka123@naver.com>
2024-01-21 17:27:27 +0900

    Feat: add modal component

---
+++ src/components/movie/ModalDetail.js
@@ -0,0 +1,11 @@
+function ModalDetail() {
+  return (
+    <div>
+      <img alt="영화 이미지" />
+      <p>영화 제목</p>
+      <p>영화 상세</p>
+    </div>
+  );
+}
+
+export default ModalDetail;
---
+++ src/components/series/ModalDetail.js
@@ -0,0 +1,11 @@
+function ModalDetail() {
+  return (
+    <div>
+      <img alt="시리즈 이미지" />
+      <p>시리즈 제목</p>
+      <p>시리즈 상세</p>
+    </div>
+  );
+}
+
+export default ModalDetail;
