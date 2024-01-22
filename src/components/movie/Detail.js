3fd192c487329e1caa448029be7ad3c31a71e8bd
chobolevel <rodaka123@naver.com>
2024-01-23 00:49:37 +0900

    Feat: add movie detail page and style

---
+++ src/components/movie/Detail.js
@@ -0,0 +1,47 @@
+function Detail({ movie }) {
+  console.log(movie)
+  return (
+    <div className="movie-detail-wrapper">
+      <div
+        className="movie-detail-inner"
+        style={{ background: `url(${movie.backDropPath})` }}
+      >
+        <div className="movie-detail-poster">
+          <img src={movie.posterPath} alt={movie.title} />
+        </div>
+        <div className="movie-detail-info">
+          <p className="movie-title">{movie.title}</p>
+          <p>{movie.overview}</p>
+          <p>
+            <span>개봉일자</span>
+            <span>{movie.releaseDate}</span>
+          </p>
+          <p>
+            <span>런타임</span>
+            <span>{movie.runtime}</span>
+          </p>
+          <p>
+            <span>관람 연령</span>
+            <span>{movie.isAdult ? "성인 관람가" : "전체 관람가"}</span>
+          </p>
+          <p>
+            <span>장르</span>
+            <span>{movie.genres?.join(", ")}</span>
+          </p>
+          <p>
+            <span>추천율</span>
+            <span>
+              {movie.voteAverage}%({movie.voteCount})
+            </span>
+          </p>
+          <p>
+            <span>제작사</span>
+            <span>{movie.companies?.map((c) => c.name).join(", ")}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Detail
--- src/routes/movie/Detail.js
+++ src/routes/movie/Detail.js
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react"
-import MovieDetail from "../../components/movie/Detail"
 import { useParams } from "react-router-dom"
+import MovieDetail from "../../components/movie/Detail"
+import MovieList from "../../components/movie/List"
+import TrailerList from "../../components/trailer/List"
 import TmdbEnum from "../../enums/TmdbEnum"
 import Movie from "../../classes/Movie"
 
 function Detail() {
   const { id } = useParams()
   const [movie, setMovie] = useState({})
+  const [trailerList, setTrailerList] = useState([])
+  const [similarMovieList, setSimilarMovieList] = useState([])
+  const [recommendationMovieList, setRecommendationMovieList] = useState([])
   async function fetchMovieDetail() {
-    const movie = await fetch(
+    const res = await fetch(
       `https://api.themoviedb.org/3/movie/${id}?language=${TmdbEnum.LANGUAGE}`,
       {
         headers: {
@@ -17,7 +22,12 @@ function Detail() {
         },
       }
     )
-    const videos = await fetch(
+
+    const json = await res.json()
+    setMovie(new Movie(json))
+  }
+  async function fetchTrailerList() {
+    const res = await fetch(
       `https://api.themoviedb.org/3/movie/${id}/videos?language=${TmdbEnum.LANGUAGE}`,
       {
         headers: {
@@ -26,15 +36,55 @@ function Detail() {
         },
       }
     )
-    const movieJson = await movie.json()
-    const videosJson = await videos.json()
-    movieJson.videos = videosJson.results
-    setMovie(new Movie(movieJson))
+    const json = await res.json()
+    setTrailerList(json.results)
+  }
+  async function fetchSimilarMovieList() {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=${TmdbEnum.LANGUAGE}`,
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: process.env.REACT_APP_TMDB_ACCESS_TOKEN,
+        },
+      }
+    )
+    const json = await res.json()
+    setSimilarMovieList(json.results.map((movie) => new Movie(movie)))
+  }
+  async function fetchRecommendationMovieList() {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/recommendations?language=${TmdbEnum.LANGUAGE}`,
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: process.env.REACT_APP_TMDB_ACCESS_TOKEN,
+        },
+      }
+    )
+    const json = await res.json()
+    setRecommendationMovieList(json.results.map((movie) => new Movie(movie)))
   }
   useEffect(() => {
     fetchMovieDetail()
+    fetchTrailerList()
+    fetchSimilarMovieList()
+    fetchRecommendationMovieList()
   }, [id])
-  return <MovieDetail movie={movie} />
+  return (
+    <>
+      <MovieDetail movie={movie} />
+      {trailerList.length !== 0 ? (
+        <TrailerList trailerList={trailerList} />
+      ) : null}
+      {similarMovieList.length !== 0 ? (
+        <MovieList title={"비슷한 영화"} movieList={similarMovieList} />
+      ) : null}
+      {recommendationMovieList.length !== 0 ? (
+        <MovieList title={"추천 영화"} movieList={recommendationMovieList} />
+      ) : null}
+    </>
+  )
 }
 
 export default Detail
