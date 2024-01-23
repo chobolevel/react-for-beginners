5e849bdc320727db768c48e9b09ad7cd86cfa3f6
chobolevel <rodaka123@naver.com>
2024-01-23 17:15:28 +0900

    Refactor: code formatting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,19 @@
-import { useEffect, useState } from "react";
-import MovieList from "../components/movie/List";
-import TmdbEnum from "../enums/TmdbEnum";
-import Movie from "../classes/Movie";
-import SeriesList from "../components/series/List";
-import Series from "../classes/Series";
+import { useEffect, useState } from "react"
+import MovieList from "../components/movie/List"
+import TmdbEnum from "../enums/TmdbEnum"
+import Movie from "../classes/Movie"
+import SeriesList from "../components/series/List"
+import Series from "../classes/Series"
 
 function Home() {
-  const [loading, setLoading] = useState(false);
-  const [popularMovieList, setPopularMovieList] = useState([]);
-  const [topRatedMovieList, setTopRatedMovieList] = useState([]);
-  const [popularSeriesList, setPopularSeriesList] = useState([]);
-  const [topRatedSeriesList, setTopRatedSeriesList] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [popularMovieList, setPopularMovieList] = useState([])
+  const [topRatedMovieList, setTopRatedMovieList] = useState([])
+  const [popularSeriesList, setPopularSeriesList] = useState([])
+  const [topRatedSeriesList, setTopRatedSeriesList] = useState([])
+  const handleClick = () => {
+    alert("아직 지원하지 않는 기능입니다.")
+  }
   async function fetchPopularMovieList() {
     const res = await fetch(
       `https://api.themoviedb.org/3/movie/popular?language=${TmdbEnum.LANGUAGE}&page=1&limit=20`,
@@ -20,9 +23,9 @@ function Home() {
           Authorization: process.env.REACT_APP_TMDB_ACCESS_TOKEN,
         },
       }
-    );
-    const json = await res.json();
-    setPopularMovieList(json.results.map((movie) => new Movie(movie)));
+    )
+    const json = await res.json()
+    setPopularMovieList(json.results.map((movie) => new Movie(movie)))
   }
   async function fetchTopRatedMovieList() {
     const res = await fetch(
@@ -33,9 +36,9 @@ function Home() {
           Authorization: process.env.REACT_APP_TMDB_ACCESS_TOKEN,
         },
       }
-    );
-    const json = await res.json();
-    setTopRatedMovieList(json.results.map((movie) => new Movie(movie)));
+    )
+    const json = await res.json()
+    setTopRatedMovieList(json.results.map((movie) => new Movie(movie)))
   }
   async function fetchPopularSeriesList() {
     const res = await fetch(
@@ -46,9 +49,9 @@ function Home() {
           Authorization: process.env.REACT_APP_TMDB_ACCESS_TOKEN,
         },
       }
-    );
-    const json = await res.json();
-    setPopularSeriesList(json.results.map((series) => new Series(series)));
+    )
+    const json = await res.json()
+    setPopularSeriesList(json.results.map((series) => new Series(series)))
   }
   async function fetchTopRatedSeriesList() {
     const res = await fetch(
@@ -59,18 +62,18 @@ function Home() {
           Authorization: process.env.REACT_APP_TMDB_ACCESS_TOKEN,
         },
       }
-    );
-    const json = await res.json();
-    setTopRatedSeriesList(json.results.map((series) => new Series(series)));
+    )
+    const json = await res.json()
+    setTopRatedSeriesList(json.results.map((series) => new Series(series)))
   }
   useEffect(() => {
-    setLoading(true);
-    fetchPopularMovieList();
-    fetchTopRatedMovieList();
-    fetchPopularSeriesList();
-    fetchTopRatedSeriesList();
-    setLoading(false);
-  }, []);
+    setLoading(true)
+    fetchPopularMovieList()
+    fetchTopRatedMovieList()
+    fetchPopularSeriesList()
+    fetchTopRatedSeriesList()
+    setLoading(false)
+  }, [])
   return (
     <>
       {loading ? "loading..." : null}
@@ -85,7 +88,9 @@ function Home() {
             과로에 시달리는 마이클 뉴먼은 인생의 장면들을 잠시 정지하고
             빨리감기할 수 있는 리모컨을 우연히 발견하는데...
           </p>
-          <button className="vignette-btn">재생하기</button>
+          <button className="vignette-btn" onClick={handleClick}>
+            재생하기
+          </button>
         </div>
       </div>
       <MovieList title={"인기있는 영화"} movieList={popularMovieList} />
@@ -96,7 +101,7 @@ function Home() {
         seriesList={topRatedSeriesList}
       />
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
