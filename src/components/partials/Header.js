2191bc3686893cc936c004453e468f9b04ff4159
chobolevel <rodaka123@naver.com>
2024-01-24 01:39:44 +0900

    Refactor: modify header structure

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,30 +1,32 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 function Header({ isTop }) {
   const handleClick = (e) => {
     e.preventDefault()
-    alert("아직 지원하지 않는 기능입니다.\n추후 지원 예정입니다.")
+    alert('아직 지원하지 않는 기능입니다.\n추후 지원 예정입니다.')
   }
   return (
-    <div className={isTop ? "header top-position" : "header"}>
-      <ul>
-        <li>
-          <Link to="/">InJae</Link>
-        </li>
-        <li>
-          <Link to="/movie" onClick={handleClick}>
-            영화
-          </Link>
-        </li>
-        <li>
-          <Link to="/series" onClick={handleClick}>
-            시리즈
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        <li onClick={handleClick}>검색</li>
-      </ul>
+    <div className={isTop ? 'header-wrapper top-position' : 'header-wrapper'}>
+      <div className="header-inner">
+        <ul>
+          <li>
+            <Link to="/">InJae</Link>
+          </li>
+          <li>
+            <Link to="/movie" onClick={handleClick}>
+              영화
+            </Link>
+          </li>
+          <li>
+            <Link to="/series" onClick={handleClick}>
+              시리즈
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li onClick={handleClick}>검색</li>
+        </ul>
+      </div>
     </div>
   )
 }
